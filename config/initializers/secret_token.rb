1245a8c9dc503ebffca82f70a8002d5a442d7387
Ralph Reid <beresfordjunior@me.com>
2013-12-05 10:16:20 +0000

    configured initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BirdDog::Application.config.secret_token = '86c3de323d12d31e6be355d30518ed52a4d4975b8d8c2289631f91a9e04ceaa74522d30bab78421f73a01e031332dd7d3f50a4fcd6988e313e64a8c05d8400c2'
