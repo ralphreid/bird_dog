374e1b076888f17995ee21b8a792cd3736f205b9
Ralph Reid <beresfordjunior@me.com>
2013-12-11 13:01:22 +0000

    completed property edit, show & update views + actions

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -5,7 +5,35 @@ class PropertiesController < ApplicationController
   end
 
   def new
-    @properties = Property.new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(params[:property])
+    if @property.save
+      redirect_to @property, notice: 'Property was successfully created!'
+    else
+      flash.now[:alert] = 'Property was NOT created!'
+      render :new
+    end
+  end
+
+  def show
+    @property = Property.find(params[:id])
+  end
+
+  def edit
+    @property = Property.find(params[:id])
+  end
+
+  def update
+    @property = Property.find(params[:id])
+    if @property.update_attributes(params[:property])
+      redirect_to @property, notice: 'Property was successfully updated!'
+    else
+      render :edit
+      flash.now[:alert] = 'Property was NOT updated!'
+    end
   end
 
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,5 @@
 class Property < ActiveRecord::Base
-  attr_accessible :arrange_view_link, :image_listing, :listing_blurb, :property_page, :rent, :rent_frequency, :source, :terms, :type, :property_source_id
+  attr_accessible :arrange_view_link, :image_listing, :listing_blurb, :property_page, :rent, :rent_frequency, :source, :terms, :type_listing, :property_source_id
 
   has_many :hunt_properties
   has_many :hunts, :through => :hunt_properties
