76357b521d575f633f0a7ede5442f8a11c2699db
Ralph Reid <beresfordjunior@me.com>
2013-12-12 20:29:05 +0000

    attempt to solve listing issue

--- app/controllers/hunts_controller.rb
+++ app/controllers/hunts_controller.rb
@@ -3,6 +3,8 @@ class HuntsController < ApplicationController
   def index
     @hunts = current_user.hunts
     @hunts_properties = current_user.hunts.map{ |hunt| hunt.hunt_properties }.flatten
+
+
     @hunts_streets = current_user.hunts.map{ |hunt| hunt.hunt_streets }.flatten
   end
 
