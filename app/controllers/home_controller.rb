bfde54288be9571d166c7e69314d5756bb8f13dc
Ralph Reid <beresfordjunior@me.com>
2013-12-19 10:08:59 +0000

    carosel implemented

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,17 @@ class HomeController < ApplicationController
 
   def index
 
+    sample = Property.order('price DESC').limit(30)
+    set = sample.map do |prop|
+      prop.image_url
+    end
+
+    set.delete("")
+    
+    @slider = set
+    
+
   end
+
+
 end
