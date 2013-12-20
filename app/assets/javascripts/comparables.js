90b10f61bf76859c8efecc866e28a9d95eb06bde
Ralph Reid <beresfordjunior@me.com>
2013-12-20 02:10:34 +0000

    updated css and gem

--- app/assets/javascripts/comparables.js
+++ app/assets/javascripts/comparables.js
@@ -3,6 +3,5 @@ $("#add_comparable").on "click", ->
   ids = []
   property_id = $(this).data("id")
   $.each checked, (index, element) -> ids.push $(element).data("property-id")
-  // console.log("ids", ids)
   $.post("/properties/"+property_id+"/add_comparable", { ids: ids, property_id: property_id })
   window.location.href = "/properties/#{id}"
